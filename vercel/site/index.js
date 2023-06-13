a9b173eac45ede443632a238623cdfe5d77ddd98
mboschetti <mboschetti@itba.edu.ar>
2023-06-14 00:13:46 +0200

    Simple recipe-into-card landing page

--- vercel/site/index.js
+++ vercel/site/index.js
@@ -9,12 +9,20 @@ $.getJSON("/api/recipes/random", events => {
         console.log(recipe);
         var tagsHtml = "";
         recipe.tags.split(",").forEach(tag => {
-            tagsHtml += `<span>` + tag + `</span>`;
+            tagsHtml += `<span class="badge rounded-pill badge-secondary">` + tag + `</span>`;
 
         })
 
+
+        var cuisineVal = "International";
+        if (recipe.cuisine_type && recipe.cuisine_type != "None") {
+            cuisineVal = recipe.cuisine_type;
+        }
+
+        var cuisineChipHtml = `<span class="badge rounded-pill badge-primary">` + cuisineVal + `</span>`;
+
         var eventCardHTML = `
-        <div class="col-lg-4 col-md-12 mb-4">
+        <div class="col-lg-3 col-md-6 mb-3">
         <div class="card">
             <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
                 <img src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" class="img-fluid" />
@@ -23,30 +31,40 @@ $.getJSON("/api/recipes/random", events => {
                 </a>
             </div>
             <div class="card-body">
-                <h5 class="card-title">`+ recipe.name + `</h5>
+                <h5 class="card-title recipe-title">`+ recipe.name + `</h5>
                 <hr class="my-4" />
-
-                <h5 class="card-title">
-                    <i class="fa-solid fa-money-bills" style="color: #005cfa;"></i> 
-                    `+recipe.budget+ `/5
-                </h5>
-
-                <h5 class="card-title">
-                    <i class="fa-regular fa-clock"></i>
-                    `+recipe.cooking_time+ `
-                </h5>
-                <h5 class="card-title">
-                    <span class="badge rounded-pill badge-primary">
-                        `+recipe.cuisine_type+ `
-                    </span>
-                </h5>
-                <h5 class="card-title">dietary_restrictions: `+recipe.dietary_restrictions+ `</h5>
-                <h5 class="card-title">difficulty_level: `+recipe.difficulty_level+ `</h5>
-                <h5 class="card-title">id: `+recipe.id+ `</h5>
-                <h5 class="card-title">preparation_time: `+recipe.preparation_time+ `</h5>
-                <h5 class="card-title">serving_size: `+recipe.serving_size+ `</h5>
-                <h5 class="card-title">tags: `+recipe.tags+ `</h5>
-
+                <p class="cuisine-type-chip">`+ cuisineChipHtml + `</p>
+                <div class="row recipe-card-row">
+                    <div class="col-4">
+                        <p class="card-title">
+                            <i class="fa-solid fa-money-bills" style="color: #009b29;"></i> 
+                            `+ recipe.budget + `/10
+                        </p>
+                    </div>
+                    <div class="col-8">
+                        <p class="card-title">
+                            <i class="fa-regular fa-clock" style="color: #3400ff;"></i>
+                            `+ recipe.cooking_time + `
+                        </p>
+                    </div>
+                </div>
+                <div class="row recipe-card-row">
+                    <div class="col-4">
+                        <p class="card-title">
+                            <i class="fa-solid fa-dumbbell" style="color: #ffbe00;"></i>
+                            `+ recipe.difficulty_level + `/10
+                        </p>
+                    </div>
+                    <div class="col-8">   
+                        <p class="card-title">
+                            <i class="fa-solid fa-plate-wheat" style="color: #00e1ff;"></i>
+                            `+ recipe.serving_size + `
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer text-muted">
+                <p class="card-text recipe-ingredients-footer">`+ recipe.ingredients + `</h5>
             </div>
         </div>
     </div>
