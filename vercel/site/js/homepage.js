aba8a71ffbd35eec5bf9b9431188020f8862060d
trittoonn <marcoo.boschetti@gmail.com>
2023-07-03 21:28:00 -0300

    Initial UX proposal

--- vercel/site/js/homepage.js
+++ vercel/site/js/homepage.js
@@ -5,10 +5,15 @@ $(document).ready(function () {
 function loadRandomRecipes() {
     $.getJSON("/api/recipes/random", events => {
 
-        var recipesHTML = "";
-        events.forEach(recipe => {
+        var recipesHTML = '<div class="row row-cols-4 text-center">';
+        var idx = 0;
+        $.each(events, function(key, recipe) {
             recipesHTML += recipeToCardHTML(recipe);
+            if(key == 3){
+                recipesHTML +='</div><div class="row row-cols-4 text-center">';
+            }
         })
+        recipesHTML +='</div>';
         $('#random-recipes').html(recipesHTML);
     });
 
--- vercel/site/js/recipes_common.js
+++ vercel/site/js/recipes_common.js
@@ -1,6 +1,29 @@
 var totalIngredients;
 
+
 function recipeToCardHTML(recipe) {
+  var cuisineVal = "International";
+  if (recipe.cuisine_type && recipe.cuisine_type != "None") {
+      cuisineVal = recipe.cuisine_type;
+  }
+
+  var imgFileName = "recipe_"+ recipe.name.toLowerCase().replaceAll(" ","_")+".png"
+  var cardHTML = `
+  <div class="card">
+    <img class="card-img-top" src="/site/img/recipes/`+imgFileName+`" alt="Unsplash">
+    <div class="card-header px-4 pt-4">
+      <div class="badge bg-warning my-2">`+ cuisineVal + `</div>
+      <h5 class="card-title mb-0 two-lines">`+ recipe.name + `</h5>
+    </div>
+    <!--<div class="card-body px-2 pt-1"></div>-->
+    <a href="#recipe_details?id=`+recipe.id+`" class="btn btn-primary">Check recipe</a>
+  </div>
+`;
+  return cardHTML;
+}
+
+
+function XrecipeToCardHTML(recipe) {
     var cuisineVal = "International";
     if (recipe.cuisine_type && recipe.cuisine_type != "None") {
         cuisineVal = recipe.cuisine_type;
@@ -15,7 +38,7 @@ function recipeToCardHTML(recipe) {
       </div>
       <div class="card-body">
         <span class="badge bg-secondary">`+ cuisineVal + `</span>
-        <a href="#" class="btn btn-primary">Check recipe</a>
+        <a href="#recipe_details?id=`+recipe.id+`" class="btn btn-primary">Check recipe</a>
       </div>
     </div>
 `;
